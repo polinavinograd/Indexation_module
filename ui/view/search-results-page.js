67f9589e373cc283ced88e54c401126620055551
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-10-09 15:05:38 +0300

    search results table header row

--- ui/view/search-results-page.js
+++ ui/view/search-results-page.js
@@ -23,40 +23,49 @@ class SearchResultsPage extends Page {
             attributes: {
                 class: "search-results-table"
             },
-            children: docArray.map(doc => ViewUtils.tag({
-                name: "tr",
-                children: [
-                    ViewUtils.tag({ name: "td", text: doc.name }),
-                    ViewUtils.tag({
-                        name: "td",
-                        child: ViewUtils.tag({
-                            name: "ul",
+            children: [
+                ViewUtils.tag({
+                    name: "tr",
+                    children: [
+                        "Название документа",
+                        "Наиболее часто встречающиеся слова",
+                        "Релевантность",
+                        "Дата добавления",
+                        "Первые <=300 символов"
+                    ].map(columnHeader => ViewUtils.tag({ name: "th", text: columnHeader }))
+                }),
+                ...docArray.map(doc => ViewUtils.tag({
+                    name: "tr",
+                    children: [
+                        ViewUtils.tag({ name: "td", text: doc.name }),
+                        ViewUtils.tag({
+                            name: "td",
                             children: doc.topWords.map(wordWeightObj => ViewUtils.tag({
-                                name: "li",
+                                name: "p",
                                 text: `${wordWeightObj.word}: ${wordWeightObj.weightCoef}`
                             }))
-                        })
-                    }),
-                    ViewUtils.tag({ name: "td", text: doc.relevance }),
-                    ViewUtils.tag({ name: "td", text: doc.date }),
-                    ViewUtils.tag({ name: "td", text: doc.snippet })
-                ],
-                eventListeners: {
-                    click: () => {
-                        // httpClient.get({
-                        //     path: "text",
-                        //     body: {
-                        //         docName: doc.name
-                        //     }
-                        // })
-                        Promise.resolve('This is the full text of a document.')
-                            .then(response => {
-                                viewDocumentPage.buildPage(response);
-                                router.navigate(viewDocumentPage);
-                            });
+                        }),
+                        ViewUtils.tag({ name: "td", text: doc.relevance }),
+                        ViewUtils.tag({ name: "td", text: doc.date }),
+                        ViewUtils.tag({ name: "td", text: doc.snippet })
+                    ],
+                    eventListeners: {
+                        click: () => {
+                            // httpClient.get({
+                            //     path: "text",
+                            //     body: {
+                            //         docName: doc.name
+                            //     }
+                            // })
+                            Promise.resolve('This is the full text of a document.')
+                                .then(response => {
+                                    viewDocumentPage.buildPage(response);
+                                    router.navigate(viewDocumentPage);
+                                });
+                        }
                     }
-                }
-            })),
+                })),
+            ]
         });
 
         this._page = ViewUtils.tag({
