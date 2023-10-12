5466c4518041adcf57f5be3e1fca46a5035ce96d
Polina <polinavinograd40@gmail.com>
2023-10-12 05:11:27 +0300

    refactored code

--- back/indexation_module.py
+++ back/indexation_module.py
@@ -1,42 +1,36 @@
 import math
 import nltk
-from logic_strategy import normalize_text, folder_path, documents_after_logic_search, delete_conjunctions, tokenize_query
 import os
+from logic_strategy import normalize_text, folder_path, documents_after_logic_search, delete_conjunctions, \
+    tokenize_query
 
 files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
 
 # поиск инверсной частоты элемента запроса element
-def find_inverse_frequency(element: str) -> float:
+def find_inverse_frequency(term: str) -> float:
     file_count = 0
     for file in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file)
-        if os.path.isfile(file_path):
-            try:
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    file_contents = f.read()
-                    text = [tag[0] for tag in normalize_text(nltk.word_tokenize(file_contents))]
-                    if element in file_contents:
-                        file_count += 1
-                    elif element in text:
-                        file_count += 1
-            except Exception as e:
-                print(f"Ошибка при обработке файла {file_path}: {str(e)}")
+        file_contents = get_file_contents(file)
+        normalized_text = [tag[0] for tag in normalize_text(nltk.word_tokenize(file_contents))]
+        if term in file_contents:
+            file_count += 1
+        elif term in normalized_text:
+            file_count += 1
     inverse_frequency = math.log(len(files)/file_count)
     return inverse_frequency
 
 
-# поиск весов элементов запроса
-def find_weights_of_query_elements(query: list, text: str) -> dict:
+def find_weights_of_query_elements(tokenized_query: list, file_contents: str) -> dict:
     weights = {}
-    for element in delete_conjunctions(query):
-        norm_text = [tag[0] for tag in normalize_text(nltk.word_tokenize(text))]
-        if " " in element:
-            q = text.count(element)/len(text.split())
+    normalized_text = [tag[0] for tag in normalize_text(nltk.word_tokenize(file_contents))]
+    for term in delete_conjunctions(tokenized_query):
+        if " " in term:
+            term_local_frequency = file_contents.count(term)/len(file_contents.split())
         else:
-            q = norm_text.count(element)/len(norm_text)
-        weight = q * find_inverse_frequency(element)
-        weights.update({element: weight})
+            term_local_frequency = normalized_text.count(term)/len(normalized_text)
+        weight = term_local_frequency * find_inverse_frequency(term)
+        weights.update({term: weight})
     return weights
 
 
@@ -51,19 +45,19 @@ def get_file_contents(file_name: str) -> str:
             print(f"Ошибка при обработке файла {file_path}: {str(e)}")
 
 
-def get_sorted_relevant_docs(query: list) -> list:
-    docs = documents_after_logic_search(query)
+def get_sorted_relevant_docs(tokenized_query: list) -> list:
+    docs = documents_after_logic_search(tokenized_query)
     docs_for_sorting = {}
     for file in docs:
-        text = get_file_contents(file)
-        docs_for_sorting.update({file: sum(find_weights_of_query_elements(query, text).values())})
+        file_contents = get_file_contents(file)
+        docs_for_sorting.update({file: sum(find_weights_of_query_elements(tokenized_query, file_contents).values())})
     sorted_docs = sorted(docs_for_sorting.items(), key=lambda item: item[1], reverse=True)
     return [doc[0] for doc in sorted_docs]
 
 
 if __name__ == "__main__":
     string = '"red roses" or "new document about"'
-    text = 'red roses'
+    # text = 'red roses'
     query = tokenize_query(string)
     print(get_sorted_relevant_docs(query))
     # print(find_inverse_frequency(['roses', 'new document about']))
--- back/logic_strategy.py
+++ back/logic_strategy.py
@@ -11,32 +11,33 @@ nltk.download('wordnet')
 
 # ключевые слова поиска, подлежащие удалению
 removable_tokens = ["find", "document", 'article', 'text', 'papers', "information", "data", "be"]
+
 lemmatizer = WordNetLemmatizer()
 
 
 # токенизация точных формулировок из запроса:
 # In: '"red roses" or "new document about"'
 # Out: ['red roses', 'or', 'new document about']
-def exact_wording(query: str) -> list:
-    tokenized = [s.strip('"') for s in re.findall(r'"[^"]*"|\S+', query.lower())]
-    return tokenized
+def exact_wording(input_string: str) -> list:
+    tokenized_query = [s.strip('"') for s in re.findall(r'"[^"]*"|\S+', input_string.lower())]
+    return tokenized_query
 
 
 # нормализация (лемматизация) текста
-def normalize_text(tokenized: list) -> list:
-    pos_tags = nltk.pos_tag(tokenized)
-    new_pos_tags = []
-    for tag in pos_tags:
-        wn_pos = wordnet.ADJ
+def normalize_text(tokenized_query: list) -> list:
+    part_of_speech_tags = nltk.pos_tag(tokenized_query)
+    new_part_of_speech_tags = []
+    for tag in part_of_speech_tags:
+        wn_part_of_speech = wordnet.ADJ
         if tag[1].startswith('N'):
-            wn_pos = wordnet.NOUN
+            wn_part_of_speech = wordnet.NOUN
         elif tag[1].startswith('V'):
-            wn_pos = wordnet.VERB
+            wn_part_of_speech = wordnet.VERB
         elif tag[1].startswith('R'):
-            wn_pos = wordnet.ADV
-        new_tag = tuple([lemmatizer.lemmatize(tag[0], wn_pos), tag[1]])
-        new_pos_tags.append(new_tag)
-    return new_pos_tags
+            wn_part_of_speech = wordnet.ADV
+        new_tag = tuple([lemmatizer.lemmatize(tag[0], wn_part_of_speech), tag[1]])
+        new_part_of_speech_tags.append(new_tag)
+    return new_part_of_speech_tags
 
 
 # удаление вспомогательных частей речи(кроме союзов) и ключевых слов поиска
@@ -52,41 +53,39 @@ def remove_tokens(normalized_tags: list, need_to_remove_keywords: bool = False) 
 
 
 # удаление союзов из запроса для process_text(query)
-def delete_conjunctions(query: list):
-    res = [el for el in query if el not in ['and', 'or', 'not']]
-    return res
+def delete_conjunctions(tokenized_query: list):
+    result_query = [term for term in tokenized_query if term not in ['and', 'or', 'not']]
+    return result_query
 
 
-def check_tokens_in_text(query: list, text: str) -> dict:
+def check_tokens_in_text(tokenized_query: list, file_contents: str) -> dict:
     res = {}
-    added_keys = set()
-    for el in query:
-        if text.find(el) != -1:
-            res.update({el: True})
-            added_keys.add(el)
-    not_added_keys = list(set(query).difference(added_keys))
-    tokenized = nltk.word_tokenize(text)
-    text = [tag[0] for tag in normalize_text(tokenized)]
-    for key in not_added_keys:
-        if key in text:
-            res.update({key: True})
+    added_terms = set()
+    for term in tokenized_query:
+        if file_contents.find(term) != -1:
+            res.update({term: True})
+            added_terms.add(term)
+    not_added_terms = list(set(tokenized_query).difference(added_terms))
+    file_contents = [tag[0] for tag in normalize_text(nltk.word_tokenize(file_contents))]
+    for term in not_added_terms:
+        if term in file_contents:
+            res.update({term: True})
         else:
-            res.update({key: False})
+            res.update({term: False})
     return res
 
 
-def tokenize_query(query: str) -> list:
-    if query.count('"') > 1:
-        tokenized_query = remove_tokens(normalize_text(exact_wording(query)), True)
+def tokenize_query(input_string: str) -> list:
+    if input_string.count('"') > 1:
+        tokenized_query = remove_tokens(normalize_text(exact_wording(input_string)), True)
     else:
-        tokenized = nltk.word_tokenize(query)
-        tokenized_query = remove_tokens(normalize_text(tokenized), True)
+        tokenized_query = remove_tokens(normalize_text(nltk.word_tokenize(input_string)), True)
     return tokenized_query
 
 
-def check_logic_strategy(text: str, tokenized_query: list):
+def check_logic_strategy(file_contents: str, tokenized_query: list):
     tokenized_query_without_conjunctions = delete_conjunctions(tokenized_query)
-    text_dict = check_tokens_in_text(tokenized_query_without_conjunctions, text)
+    query_dict = check_tokens_in_text(tokenized_query_without_conjunctions, file_contents)
     prev_word = ""
     res = {}
     or_statements = []
@@ -104,12 +103,12 @@ def check_logic_strategy(text: str, tokenized_query: list):
         prev_word = word
 
     for key in res.keys():
-        if res[key] != text_dict[key]:
+        if res[key] != query_dict[key]:
             return False
-    return any(text_dict[key] is True for key in or_statements) if len(or_statements) != 0 else True
+    return any(query_dict[term] for term in or_statements) if len(or_statements) != 0 else True
 
 
-def documents_after_logic_search(query: list) -> list:
+def documents_after_logic_search(tokenized_query: list) -> list:
     relevant_docs = []
     for file in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file)
@@ -117,7 +116,7 @@ def documents_after_logic_search(query: list) -> list:
             try:
                 with open(file_path, 'r', encoding='utf-8') as f:
                     file_contents = f.read()
-                    if check_logic_strategy(file_contents, query):
+                    if check_logic_strategy(file_contents, tokenized_query):
                         relevant_docs.append(file)
             except Exception as e:
                 print(f"Ошибка при обработке файла {file_path}: {str(e)}")
@@ -131,6 +130,7 @@ if __name__ == "__main__":
 
     string = '"red roses" or "new document about"'
     text = 'red roses'
+    print(documents_after_logic_search(tokenize_query(string)))
     # text = 'roses are red'
     # text = 'new roses'
 
